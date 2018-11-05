933ba1811c7d6d4423731bb82305f477cb29031d
Cole Marsteller and Miles Bradt <miles@>
2018-11-05 14:36:22 -0800

    basic falling box animation

--- js/falling-obects.js
+++ js/falling-obects.js
@@ -1,11 +1,81 @@
 // https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
 
+
 var canvas = document.querySelector('canvas');
 
 var ctx = canvas.getContext('2d');
 
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
+
+var myGameArea = { // makes canvas parameters. canvas is an html element that only takes images, and graphic from JavaScript.
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = width;
+        this.canvas.height = height;
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        });
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        });
+    },
+    clear : function(){
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    } // clears the canvas of old versions of objects (like where the pc was one key press ago)
+}
+
+function updateGameArea() { // draws the new position of the pc after removing ALL objects in canvas.
+    myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+    if (myGameArea.key && myGameArea.key == 37) {
+      myGamePiece.speedX = -5;
+     }
+    if (myGameArea.key && myGameArea.key == 39) {
+      myGamePiece.speedX = 5;
+     }
+    if (myGameArea.key && myGameArea.key == 38) {
+      myGamePiece.speedY = -5;
+     }
+    if (myGameArea.key && myGameArea.key == 40) {
+      myGamePiece.speedY = 5;
+     }
+    myGamePiece.newPos();
+    myGamePiece.update();
+
+    for (var i = 0; i < balls.length; i++) {
+      balls[i].draw();
+      balls[i].update();
+
+    }
+}
+
+function component(width, height, color, x, y) {  // object with
+  this.gamearea = myGameArea;
+  this.width = width;
+  this.height = height;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.x = x;
+  this.y = y;
+  this.update = function() {
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+  this.newPos = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+}
+
+// function startGame() {  // makes pc as a component piece
+//     myGameArea.start();
+//     myGamePiece = new component(30, 30, "red", 10, 120);
+// }
 
 
 // RNG
@@ -59,6 +129,8 @@ Ball.prototype.update = function() {
 
 var balls = [];
 var testBall = new Ball(50, 100, 4, 4, 'blue');
+var myGamePiece = new component(30, 30, "red", 10, 120);
+
 
 testBall.x
 testBall.size
@@ -66,8 +138,10 @@ testBall.color
 testBall.draw()
 
 function loop() {
-  ctx.fillStyle = 'rgba(10, 10, 10, 0.7)';
+
+  ctx.fillStyle = 'rgba(10, 10, 10)';
   ctx.fillRect(0, 0, width, height);
+
 
   // Determines the amount of objects spawned on screen
   while (balls.length <= 15) {
@@ -90,9 +164,11 @@ function loop() {
   for (var i = 0; i < balls.length; i++) {
     balls[i].draw();
     balls[i].update();
+
   }
 
   requestAnimationFrame(loop);
 }
+
 
 loop();
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,131 @@
+// // Snow Falling
+// function fallingSnow() {
+//   var $snowflakes = $(),
+//   createSnowflakes = function () {
+//     var qt = 50;
+//     for (var i = 0; i < qt; ++i) {
+//       var $snowflake = $('<div class="snowflakes"></div>');
+//       $snowflake.css({
+//         'left': (Math.random() * $('#site').width()) + 'px',
+//         'top': 0
+//       });
+//       // add this snowflake to the set of snowflakes
+//       $snowflakes = $snowflakes.add($snowflake);
+//     }
+//     $('#snowZone').prepend($snowflakes);
+//   },
+//
+//   runSnowStorm = function() {
+//     $snowflakes.each(function() {
+//
+//       var singleAnimation = function($flake) {
+//         $flake.animate({
+//           top: "500px",
+//           opacity : "0",
+//         }, Math.random()*-2500 + 5000, function(){
+//           // this particular snow flake has finished, restart again
+//           $flake.css({
+//             'left': (Math.random() * $('#site').width()) + 'px',
+//             'top': 0,
+//             'opacity': 1
+//           });
+//           singleAnimation($flake);
+//         });
+//       };
+//       singleAnimation($(this));
+//     });
+//   };
+//
+//   createSnowflakes();
+//   runSnowStorm();
+// }
+// fallingSnow();
 
+// Crafty.init(750, 750);
+//
+// var dim1 = {x: 5, y: 5, w: 50, h: 50}
+// var dim2 = {x: 100, y: 100, w: 60, h: 40}
+//
+// var rect1 = Crafty.e("2D, Canvas, Color").attr(dim1).color("red");
+//
+// var rect2 = Crafty.e("2D, Canvas, Color, Keyboard, Fourway").fourway(2).attr(dim2).color("blue");
+//
+// rect2.bind("EnterFrame", function () {
+//     if (rect1.x < rect2.x + rect2.w &&
+//         rect1.x + rect1.w > rect2.x &&
+//         rect1.y < rect2.y + rect2.h &&
+//         rect1.h + rect1.y > rect2.y) {
+//         // collision detected!
+//         alert("Hit");
+//         dim2 = {x: 100, y: 100}
+//     } else {
+//         // no collision
+//         this.color("blue");
+//     }
+// });
+
+// Crafty.init(200, 200);
+//
+// var dim1 = {x: 5, y: 5}
+// var dim2 = {x: 20, y: 20}
+//
+// Crafty.c("Circle", {
+//    circle: function(radius, color) {
+//         this.radius = radius;
+//         this.w = this.h = radius * 2;
+//         this.color = color || "#000000";
+//
+//         this.bind("Move", Crafty.DrawManager.drawAll)
+//         return this;
+//    },
+//
+//    draw: function() {
+//        var ctx = Crafty.canvas.context;
+//        ctx.save();
+//        ctx.fillStyle = this.color;
+//        ctx.beginPath();
+//        ctx.arc(
+//            this.x + this.radius,
+//            this.y + this.radius,
+//            this.radius,
+//            0,
+//            Math.PI * 2
+//        );
+//        ctx.closePath();
+//        ctx.fill();
+//        ctx.restore();
+//     }
+// });
+//
+// var circle1 = Crafty.e("2D, Canvas, Circle").attr(dim1).circle(15, "red");
+//
+// var circle2 = Crafty.e("2D, Canvas, Circle, Fourway").fourway(2).attr(dim2).circle(20, "blue");
+//
+// circle2.bind("EnterFrame", function () {
+//     var dx = (circle1.x + circle1.radius) - (circle2.x + circle2.radius);
+//     var dy = (circle1.y + circle1.radius) - (circle2.y + circle2.radius);
+//     var distance = Math.sqrt(dx * dx + dy * dy);
+//
+//     if (distance < circle1.radius + circle2.radius) {
+//         // collision detected!
+//         this.color = "green";
+//     } else {
+//         // no collision
+//         this.color = "blue";
+//     }
+// });
+
+function myMove() {
+  var elem = document.getElementById("animate");
+  var pos = 0;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos >= 345) {
+      clearInterval(id);
+    } else {
+      pos += 6;
+      elem.style.top = pos + 'px';
+      // elem.style.left = pos + 'px';
+    }
+  }
+}
